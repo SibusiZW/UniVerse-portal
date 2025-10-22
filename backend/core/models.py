418def835df56aadb22358457d1df49bcfa1dbee
Tafadzwa Sibanda <sibusitf@gmail.com>
2025-10-22 18:50:26 +0200

    feat: add Student model and initial migration

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    student_id = models.CharField(max_length=10, primary_key=True)
+    student_name = models.CharField(max_length=100)
+    student_email = models.EmailField(unique=True)
+    created_at = models.DateField(auto_now_add=True)

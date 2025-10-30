049baa2b9cc64b5e8029a018762d432d780bcd4d
Tafadzwa Sibanda <sibusitf@gmail.com>
2025-10-30 15:13:23 +0200

    feat: add backend requirements, update layout metadata, enhance homepage design, and include logo

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,25 @@
-import { LogIn } from "lucide-react"
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <div>
-      <button className="bg-blue-300 p-4">
-        <LogIn size={20}/> Login
-      </button>
+    <div className="flex h-screen items-center justify-center">
+     <div className="bg-white p-10 rounded-xl shadow-2xl w-full max-w-sm text-center text-black">
+        <div className="mb-8 flex flex-col items-center">
+        <Image 
+          src={'/universe-logo.png'}
+          width={120}
+          height={40}
+          className="mb-2"
+          alt="UniVerse Logo"
+        />
+        </div>
+        <h1 className="text-xl font-semibold text-gray-500 mb-2">
+          Welcome to UniVerse Portal
+        </h1>
+
+          <input className="p-4 w-75 border border-blue-300 rounded-md mb-4" placeholder="Enter student no. e.g, ST12345678"/>
+          <input className="p-4 w-75 border border-blue-300 rounded-md" placeholder="Enter password"/>
+     </div>
     </div>
   )
 }

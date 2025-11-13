c941aef2fa74fff30a4febc9ede1ecaa89151e46
Tafadzwa Sibanda <sibusitf@gmail.com>
2025-11-13 17:55:10 +0200

    todo: You have to finish setting up the assignments page going to study📚

--- frontend/app/dashboard/assignments/page.tsx
+++ frontend/app/dashboard/assignments/page.tsx
@@ -1,9 +1,20 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { NotebookPen } from "lucide-react";
 
+interface Assignment {
+    assignment_id: string,
+    course: string,
+    issued_by: string
+    // TODO: Finish setting up interface for assignments
+}
+
 export default function AssignmentsPage() {
+
+
     return (
         <NotebookPen className="mb-6" size={100}/>
+        // TODO: Write markup and JS logic for displaying available assignments
     )
 }
